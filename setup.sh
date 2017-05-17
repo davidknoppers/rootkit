77d89e45464a2ce1b05afb77d3e2c5a68c4d2d59
Richard Sim <rdsim8589@gmail.com>
2017-05-17 16:01:28 -0700

    added -q to all installs and a line to install sqlAlchmey

--- setup.sh
+++ setup.sh
@@ -2,17 +2,19 @@
 ## Setting up environment
 sudo apt-get update
 ## firewall
-sudo apt-get install ufw -y
+sudo apt-get install ufw -y -q
 ## editor
-sudo apt-get install emacs -y
+sudo apt-get install emacs -y -q
 ## webserver
-sudo apt-get install nginx -y
+sudo apt-get install nginx -y -q
 ## mysql
-sudo apt-get install mysql=5.7 -y
+sudo apt-get install mysql=5.7 -y -q
 ## python dependencies
-sudo apt-get install python3-dev -y
-sudo apt-get install python3-pip -y
-sudo apt-get install libmysqlclient-dev -y
-sudo pip3 install flask
-sudo pip3 install Jinga2
-sudo pip3 install mysqlclient
+sudo apt-get install python3-dev -y -q
+sudo apt-get install python3-pip -y -q
+sudo apt-get install libmysqlclient-dev -y -q
+sudo pip3 install -q flask
+sudo pip3 install -q Jinga2
+sudo pip3 install -q mysqlclient
+sudo pip3 install -q SQLAlchmeny
+sudo pip3 install -q requests
