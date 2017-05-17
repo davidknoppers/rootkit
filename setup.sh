1a4b2c16bb57bb67af8db8f6873385414d6b42ac
Richard Sim <rdsim8589@gmail.com>
2017-05-17 15:45:31 -0700

    added installing server

--- setup.sh
+++ setup.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 ## Setting up environment
 sudo apt-get update
-## editor
-sudo apt-get install emacs -y
 ## firewall
 sudo apt-get install ufw -y
+## editor
+sudo apt-get install emacs -y
+## webserver
+sudo apt-get install nginx -y
 ## mysql
 sudo apt-get install mysql=5.7 -y
 ## python dependencies
