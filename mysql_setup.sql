92cf72d42371dcc3b30a6628336bbdb1c913aae0
davidknoppers <david.knoppers@holbertonschool.com>
2017-05-17 23:26:34 +0000

    not tested draft, but ready for review

--- mysql_setup.sql
+++ mysql_setup.sql
@@ -1,7 +1,14 @@
 CREATE DATABASE IF NOT EXISTS plants_db;
 USE plants_db;
+
+CREATE TABLE IF NOT EXISTS plants(
+plant_id INT,
+plant_nickname VARCHAR(50),
+PRIMARY KEY('plant_id')
+) DEFAULT CHARSET=ut8;
+
 CREATE TABLE IF NOT EXISTS sensor_readings(
-plant_id INT FOREIGN KEY REFERENCES plants(plantID),
+plant_id INT FOREIGN KEY REFERENCES plants('plant_id'),
 date_time TIMESTAMP,
 _date DATE,
 _time TIME,
@@ -10,8 +17,3 @@ reading_value INT,
 reading_id INT NOT NULL AUTO_INCREMENT,
 PRIMARY KEY('reading_id'),
 ) DEFAULT CHARSET=utf8;
-CREATE TABLE IF NOT EXISTS plants(
-plant_id INT,
-plant_nickname VARCHAR(50),
-PRIMARY KEY('plant_id')
-) DEFAULT CHARSET=ut8;
