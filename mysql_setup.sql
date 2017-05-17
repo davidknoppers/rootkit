837c52e85918d89307cd859cf3b5d9608a7fc99f
davidknoppers <david.knoppers@holbertonschool.com>
2017-05-17 23:48:58 +0000

    untested draft, but near completion - ready for review

--- mysql_setup.sql
+++ mysql_setup.sql
@@ -1,14 +1,23 @@
+-- create the basic plants_db database and switch to it
 CREATE DATABASE IF NOT EXISTS plants_db;
 USE plants_db;
 
+-- create our user
+GRANT ALL ON *.* TO 'plant_boye'@'localhost' IDENTIFIED BY 'ppp';
+
+-- create the plants table, meant to hold plant ids and their nicknames
 CREATE TABLE IF NOT EXISTS plants(
 plant_id INT,
 plant_nickname VARCHAR(50),
 PRIMARY KEY('plant_id')
 ) DEFAULT CHARSET=ut8;
 
+
+-- create the sensor readings table, meant to hold all the readings from our arduino
+-- we declare things like reading value and type ambiguously so that we can
+-- make them flexible for later: hold different readings for moisture, temperature, etc
 CREATE TABLE IF NOT EXISTS sensor_readings(
-plant_id INT FOREIGN KEY REFERENCES plants('plant_id'),
+plant_id INT NOT NULL FOREIGN KEY REFERENCES plants('plant_id'),
 date_time TIMESTAMP,
 _date DATE,
 _time TIME,
