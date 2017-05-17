b97a491e14ed1cf0a646cb4e595d80e34040c183
davidknoppers <david.knoppers@holbertonschool.com>
2017-05-17 22:58:26 +0000

    updating with basic commands for creating second table, not a tested script

--- mysql_setup.sql
+++ mysql_setup.sql
@@ -1,7 +1,7 @@
 CREATE DATABASE IF NOT EXISTS plants_db;
 USE plants_db;
 CREATE TABLE IF NOT EXISTS sensor_readings(
-plant_id INT,
+plant_id INT FOREIGN KEY REFERENCES plants(plantID),
 date_time TIMESTAMP,
 _date DATE,
 _time TIME,
@@ -10,3 +10,8 @@ reading_value INT,
 reading_id INT NOT NULL AUTO_INCREMENT,
 PRIMARY KEY('reading_id'),
 ) DEFAULT CHARSET=utf8;
+CREATE TABLE IF NOT EXISTS plants(
+plant_id INT,
+plant_nickname VARCHAR(50),
+PRIMARY KEY('plant_id')
+) DEFAULT CHARSET=ut8;
