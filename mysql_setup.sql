dca45f8cdd4b1be7251737fbc39df32978ba8169
davidknoppers <david.knoppers@holbertonschool.com>
2017-05-18 01:58:04 +0000

    first working(?) draft of mysql server setup

--- mysql_setup.sql
+++ mysql_setup.sql
@@ -9,20 +9,21 @@ GRANT ALL ON *.* TO 'plant_boye'@'localhost' IDENTIFIED BY 'ppp';
 CREATE TABLE IF NOT EXISTS plants(
 plant_id INT,
 plant_nickname VARCHAR(50),
-PRIMARY KEY('plant_id')
-) DEFAULT CHARSET=ut8;
+PRIMARY KEY(plant_id)
+) DEFAULT CHARSET=utf8;
 
 
 -- create the sensor readings table, meant to hold all the readings from our arduino
 -- we declare things like reading value and type ambiguously so that we can
 -- make them flexible for later: hold different readings for moisture, temperature, etc
-CREATE TABLE IF NOT EXISTS sensor_readings(
-plant_id INT NOT NULL FOREIGN KEY REFERENCES plants('plant_id'),
+CREATE TABLE IF NOT EXISTS sensor_readings (
+plant_id INT NOT NULL,
 date_time TIMESTAMP,
 _date DATE,
 _time TIME,
 reading_type VARCHAR(50),
 reading_value INT,
 reading_id INT NOT NULL AUTO_INCREMENT,
-PRIMARY KEY('reading_id'),
+PRIMARY KEY(reading_id),
+FOREIGN KEY plant_id(plant_id) REFERENCES plants(plant_id)
 ) DEFAULT CHARSET=utf8;
