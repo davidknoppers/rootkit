ce5a430a0b2cfe42cd9868f931f77f823fea400d
davidknoppers <david.knoppers@holbertonschool.com>
2017-06-06 21:02:44 +0000

    adding WIP node app

--- server/express/api/v1/app.js
+++ server/express/api/v1/app.js
@@ -1,12 +1,46 @@
 #!/usr/bin/node
 
-var express = require("express");
-var myParser = require("body-parser");
-var app = express();
+let express = require("express");
+let myParser = require("body-parser");
+let parseJson = require('parse-json');
+let app = express();
 
-app.use(myParser.urlencoded({extended : true}));
-app.post("/yourpath", function(request, response) {
-console.log(request.body); //This prints the JSON document received (if it is a JSON document)
+
+app.use(myParser.json());
+app.post("/express/api/v1", function(request, response) {
+let obj = request.body;
+response.send(obj);
+console.log(JSON.stringify(obj))
+console.log(typeof(JSON.stringify(obj)))
+let json_thing = JSON.parse(JSON.stringify(obj))
+console.log('json thing: ' + json_thing.reading_value)
+console.log('type of json thing: ' + typeof(json_thing.reading_value))
 });
- //Start the server and make it listen for connections on port 8080
-app.listen(8080);
+
+let qry = "INSERT INTO readings (`reading_dt`, `reading_date`, `reading_time`, `plant_id`, `user_id`, `reading_type`, `plantgroup_id`, `reading_value`) VALUES ('2017-06-05 13:39:43', '2017-06-05', '13:39:43', 1, 1, 'soil_moisture', 1, 760.625);"
+
+
+let con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "poo",
+    database: "nodejs_test"
+});
+con.connect(function(err){
+  if(err){
+    console.log('Error connecting to Db: ' + err.stack);
+    return;
+  }
+  console.log('Connection established as ' + con.threadId);
+});
+
+con.query(qry, function (err, rows) {
+    if (err) throw err;
+    for (let i = 0; i < rows.length; i++) {
+	console.log(rows[i]);
+	};
+});
+con.end(function(err) {
+});
+app.listen(8000);
+console.log("server is listening")
