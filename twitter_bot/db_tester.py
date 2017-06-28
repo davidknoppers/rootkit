be4096b119b74cc815e5f03ed1a3825ecac46999
davidknoppers <david.knoppers@holbertonschool.com>
2017-06-28 23:28:31 +0000

    simply saving progress

--- twitter_bot/db_tester.py
+++ twitter_bot/db_tester.py
@@ -1,52 +1,64 @@
 #!/usr/bin/python3
 
-#get the keys for oauth
-from keys import *
 #MYSQLdb simply lets us access the MYSQL db
 import MySQLdb
 #we use json to keep track of what reading ids we've seen
 import json
 #we use sys to access the db in a slightly safer manner
 import sys
+
 #tweepy is our main bot engine
 import tweepy
-
-
+#get the keys for oauth
+from keys import *
 #set up oauth
 auth = tweepy.OAuthHandler(ckey,csecret)
 auth.set_access_token(akey, asecret)
-
 #launch basic api
-api = tweepy.API(auth)
+twitter_api = tweepy.API(auth)
 
-#connect to the MySQL db using credentials passed from sys.argv
-db = MySQLdb.connect(host='localhost', user=sys.argv[1],
-                     passwd=sys.argv[2], db=sys.argv[3])
+def alert_user(reading, api=twitter_api):
+    #load dictionary that holds mapping of user_ids to twitter handles
+    try:
+        with open('ids_and_handles.json') as f:
+            ids_and_handes = json.load(f)
+    except:
+        ids_and_handles = {"1": "@p0516357"}
+    reading_date = reading[2]
+    moisture_level = reading[4]
+    plant_id = reading[5]
+    user_id = reading[6]
+    message = "Alert {}! Your plant's moisture is getting low. Plant id: {} Reading date: {}".format(ids_and_handles[str(user_id)], plant_id, reading_date)
+    print(message)
+def check_db():
+    #connect to the MySQL db using credentials passed from sys.argv
+    db = MySQLdb.connect(host='localhost', user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
+    #set up a db cursor that will be used to query the database
+    cursor = db.cursor()
 
-#set up a db cursor that will be used to query the database
-cursor = db.cursor()
+    #The table has an auto-increment integer that tells us the order of the readings in the db. We simply keep track of what readings
+    #we've already seen by saving and tracking the largest id read by the program
+    #this prevents us pinging a user multiple times for the same reading
+    try:
+        with open('max_id_seen.json') as f:
+            max_id_seen = json.load(f)
+    except:
+        max_id_seen = 1
+    query = ("SELECT * FROM readings WHERE reading_id > {}").format(max_id_seen)
+    cursor.execute(query)
+    #list of reading ids that have now been "read" by the bot
+    for reading in cursor.fetchall():
+        moisture_level = reading[4]
+        if moisture_level < 400:
+            print("moisture check triggered")
+            plant_id = reading[5]
+            user_id = reading[6]
+            alert_user(reading)
+        max_id_seen = reading[0]
+    db.close()
+    #save the max id seen
+    with open('max_id_seen.json', 'w+') as f:
+        json.dump(max_id_seen, f)
 
-#query = ("SELECT * FROM readings WHERE reading_dt = (SELECT MAX(reading_dt) FROM readings)")
-#query = ("SELECT reading_id, reading_value, plant_id, user_id, reading_type FROM readings ORDER BY reading_id DESC LIMIT 10")
-#The table has an auto-increment integer that tells us the order of the readings in the db. We simply keep track of what readings
-#we've already seen by saving and tracking the largest id read by the program
-#this prevents us pinging a user multiple times for the same reading
-try:
-    with open('max_id_seen.json') as f:
-        max_id_seen = json.load(f)
-except:
-    max_id_seen = 1
-query = ("SELECT reading_id, reading_value, plant_id, user_id, reading_type FROM readings WHERE reading_id > {}").format(max_id_seen)
-
-cursor.execute(query)
-
-#list of reading ids that have now been "read" by the bot
-for row in cursor.fetchall():
-    print(row)
-    best = row[0]
-
-db.close()
-
-#save the largest id seen
-with open('data.json', 'w') as f:
-    json.dump(max_id_seen, f)
+check_db()
