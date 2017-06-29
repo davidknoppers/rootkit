fe086125e0675c3dc5c4a4172baa3051dc482caf
Richard Sim <rdsim8589@gmail.com>
2017-06-29 12:16:13 -0700

    added funtionality to display the graph

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import Chart from 'chart.js'
+import {Line as LineChart} from 'react-chartjs-2'
+import Request from 'react-http-request'
 // import "bootstrap/dist/css/bootstrap.css";
 import "bootswatch/sandstone/bootstrap.css";
 import './App.css';
@@ -10,26 +13,90 @@ const PLACES = [
   {name: 'Thai basil', zip: '94110'},
   {name: 'Rosemary', zip: '94110'}
 ];
+var chartData =
+{
+labels: [],
+datasets: [{
+    label: 'Soil Moisture',
+    data: [],
+    backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(255, 159, 64, 0.2)'
+    ],
+    borderColor: [
+        'rgba(255,99,132,1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 159, 64, 1)'
+    ],
+    borderWidth: 1
+  }]
+};
+var chartOption =
+{
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero:true
+          }
+        }]
+      }
+};
 
 class WeatherDisplay extends Component {
   constructor () {
     super();
     this.state = { weatherData: null };
+    this.handleGet = this.handleGet.bind(this);
   }
   componentDidMount () {
+    const script = document.createElement("script");
+    script.src = "https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.4.0/Chart.min.js"
+    script.async = true;
     const zip = this.props.zip;
     console.log(zip);
     const URL = 'http://api.openweathermap.org/data/2.5/weather?q=' + zip + '&appid=b1b35bba8b434a28a0be2a3e1071ae5b&units=imperial';
     fetch(URL).then(res => res.json()).then(json => {
       this.setState({ weatherData: json });
     });
+      this.handleGet();
   }
+  handleGet() {
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = (e) => {
+      if (request.readyState !==4) {
+        return;
+      }
+      if (request.status === 200) {
+        console.log('success', request.responseText);
+        var getResponse = JSON.parse(request.responseText)
+        for(var i=getResponse.length-1; i >= 0; i--) {
+          console.log("this is a test")
+          chartData.labels.push(getResponse[i]["reading_date"])
+          chartData.datasets[0].data.push(getResponse[i]["reading_value"])
+        }
+
+      } else {
+        console.warn('error')
+      }
+    };
+    request.open('GET', 'http://52.90.202.94:7000/1/1/1/10')
+    request.send();
+  };
+
   render () {
     const weatherData = this.state.weatherData;
+    var LineChart = require("react-chartjs-2").Line;
     if (!weatherData) return <div>Let me pull this up... One moment please</div>;
     return (
       <div>
-        <h1> Chart should go here </h1>
+        <h1> <LineChart data={chartData} options={chartOption} width="600" height="250"/> </h1> {/* insert the graph*/}
       </div>
     );
   }
